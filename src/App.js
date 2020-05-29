88bfd5ca5e64861160e2ebbafc3c9b1cb2918e8a
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-05-29 09:24:40 -0300

    Verificado uso do useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  return <h1>Hello World</h1>;
+  const [tech, setTech] = useState(['ReactJS', 'React Native']);
+  const [nameTech, setNameTech] = useState('');
+
+  function handleAdd() {
+    setTech([...tech, nameTech]);
+    setNameTech('');
+  }
+
+  return (
+    <>
+      <ul>
+        {tech.map((t) => (
+          <li key={t}>{t}</li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        value={nameTech}
+        onChange={(e) => setNameTech(e.target.value)}
+      />
+      <button type="button" onClick={handleAdd}>
+        Adicionar
+      </button>
+    </>
+  );
 }
 
 export default App;
