3c5049a873a8f43eb7ad1efdaf12753949ca5863
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-05-29 10:00:30 -0300

    Verificado o uso do useMemo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 
 function App() {
-  const [tech, setTech] = useState(['ReactJS', 'React Native']);
+  const [tech, setTech] = useState([]);
   const [nameTech, setNameTech] = useState('');
 
   function handleAdd() {
     setTech([...tech, nameTech]);
     setNameTech('');
   }
+
+  useEffect(() => {
+    const techs = localStorage.getItem('tech');
+
+    if (techs) {
+      setTech(JSON.parse(techs));
+    }
+
+    // Did unmount
+    // return () => {};
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(tech));
+  }, [tech]);
+
+  const techSize = useMemo(() => tech.length, [tech]);
 
   return (
     <>
@@ -16,6 +33,8 @@ function App() {
           <li key={t}>{t}</li>
         ))}
       </ul>
+      <strong>Você tem {techSize} tecnologias.</strong>
+      <br />
       <input
         type="text"
         value={nameTech}
